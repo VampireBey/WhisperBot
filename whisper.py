0ee7b3c97026c02947cf58bbe637d0d50c2d2535
VampireBey <ichbinmusto@gmail.com>
2025-09-16 05:53:39 +0300

    Update whisper.py

--- whisper.py
+++ whisper.py
@@ -3,12 +3,11 @@ from pyrogram import Client, idle
 import Config
 from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
 
-
 logging.basicConfig(
-    level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
 
 app = Client(
     "Whisper-Bot",
@@ -18,17 +17,20 @@ app = Client(
     plugins=dict(root="WhisperBot"),
 )
 
-
-# Run Bot
 if __name__ == "__main__":
     try:
-        app.start()  # Not using run as wanna print
+        app.start()
     except (ApiIdInvalid, ApiIdPublishedFlood):
-        raise Exception("Your API_ID/API_HASH is not valid.")
+        raise Exception("API_ID veya API_HASH geçersiz!")
     except AccessTokenInvalid:
-        raise Exception("Your BOT_TOKEN is not valid.")
-    uname = app.get_me().username
-    print(f"@{uname} Started Successfully!")
+        raise Exception("BOT_TOKEN geçersiz!")
+
+    try:
+        uname = app.get_me().username
+        print(f"@{uname} başarıyla başlatıldı!")
+    except Exception as e:
+        print(f"Kullanıcı bilgisi alınamadı: {e}")
+
     idle()
     app.stop()
-    print("Bot stopped. Alvida!")
+    print("Bot durduruldu. Hoşça kal!")
