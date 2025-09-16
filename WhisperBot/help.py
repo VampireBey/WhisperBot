5c2230e659d24fc0250b564181fafbb6a9f99d7c
VampireBey <ichbinmusto@gmail.com>
2025-09-16 06:05:28 +0300

    Update help.py

--- WhisperBot/help.py
+++ WhisperBot/help.py
@@ -2,12 +2,10 @@ from Data import Data
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup
 
-
-# Help Message
 @Client.on_message(filters.private & filters.incoming & filters.command("help"))
-async def _help(bot, msg):
+async def yardim(bot, msg):
     await bot.send_message(
         msg.chat.id,
-        "**Here's how to use me 🔐**\n" + Data.HELP,
+        "**Beni nasıl kullanacağını öğren 🔐**\n" + Data.HELP,
         reply_markup=InlineKeyboardMarkup(Data.home_buttons),
     )
